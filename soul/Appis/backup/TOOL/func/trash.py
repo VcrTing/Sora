c2d11115122b4a29b53f8cef88716e7142f61165
VcrTing <vcrting@163.com>
2021-09-18 15:33:06 +0800

    0

--- soul/Appis/backup/TOOL/func/trash.py
+++ soul/Appis/backup/TOOL/func/trash.py
@@ -8,31 +8,27 @@ def need_trash(f):
     ns = f.split('_')
     ns = ns[1]
     now = time_name()
+    print('删除判断，n - o =', (now - int(ns)))
     if ns:
         if (now - int(ns)) > BACK_INTERVAL:
             return True 
-    print('无需删除，n - o =', (now - int(ns)))
     return False
 
 def trash_old(named):
-    try:
-        rec = os.path.join(
-            BACK_ROOT,
-            named
-        )
-        print('执行的删除路径 =', rec)
-
-        shutil.rmtree(rec)
-    except Exception as e:
-        pass
+    rec = os.path.join(
+        BACK_ROOT,
+        named
+    )
     
-    """
-    rec = os.listdir(rec)
-    for f in rec:
-        need = need_trash(f)
+    try:
+        rec = os.listdir(rec)
+        for f in rec:
+            need = need_trash(f)
 
-        if need:
-            print('删除文件 =', f)
-            trash(f)
+            if need:
+                print('删除文件 =', f)
+                trash(f)
 
-    """
+    except Exception as e:
+        print('出错执行 通用删除')
+        shutil.rmtree(rec)
